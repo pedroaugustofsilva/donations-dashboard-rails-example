86b08431e4951d338ccbe989a69d712c4354e53b
Pedro Augusto <pedroaugusto@hey.com>
2024-01-16 07:52:36 -0300

    create campaign index layout

--- test/controllers/campaigns_controller_test.rb
+++ test/controllers/campaigns_controller_test.rb
@@ -1,8 +1,19 @@
 require "test_helper"
 
 class CampaignsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @campaign_names = Campaign.pluck(:name)
+  end
   test "should get index" do
     get campaigns_url
     assert_response :success
+  end
+
+  test "should show two campaigns" do
+    get campaigns_url
+    assert_select "section div h5", 2
+    assert_select "section div h5" do |elements|
+      assert @campaign_names.include?(elements.first.content.strip)
+    end
   end
 end
